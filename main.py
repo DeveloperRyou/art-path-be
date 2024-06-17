801cc357bf9ca515bcae6cdf7c043580c3cc81c8
DeveloperRyou <DeveloperRyou@gmail.com>
2024-06-17 18:03:15 +0900

    FEAT: fastapi init

---
+++ main.py
@@ -0,0 +1,24 @@
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.on_event("startup")
+def boot_server():
+    logging.info("server boot ...")
+
+
+@app.on_event("shutdown")
+def shutdown_server():
+    logging.info("server shutdown ...")
+
+
+@app.get("/test")
+def get_result():
+    return {"test": "success"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
