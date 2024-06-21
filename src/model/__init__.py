97cd1968b0a0d187c502b77c36fb716965b8ea3b
inatai <taitai@prog.com>
2024-06-21 19:41:34 +0900

    add IllustMetadataTable

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,9 +1,11 @@
 from .record import RecordTable
 from .route import RouteTable
 from .user import UserTable
+from .illust_metadata import IllustMetadataTable
 
 __all__ = [
     "RecordTable",
     "RouteTable",
     "UserTable",
+    "IllustMetadataTable"
 ]
