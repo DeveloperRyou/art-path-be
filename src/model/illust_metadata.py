528611ae5b95d9de172d1f77b03cf3efc74818be
inatai <taitai@prog.com>
2024-06-22 15:56:11 +0900

    update illust metadata table

--- src/model/illust_metadata.py
+++ src/model/illust_metadata.py
@@ -1,21 +1,10 @@
 from uuid import UUID
-import enum
 
 from sqlalchemy import String, JSON, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.model.db_core import Base
-
-
-class GenreEnum(enum.Enum):
-    Vehicle = "のりもの"
-    Weather_Season = "天気・季節"
-    Animal = "生き物"
-    Prefectures = "都道府県"
-    Sports = "スポーツ"
-    Symbols = "記号"
-    Other = "その他"
-
+from src.enum.genre_enum import GenreEnum
 
 
 class IllustMetadataTable(Base):
@@ -24,5 +13,6 @@ class IllustMetadataTable(Base):
     id: Mapped[UUID] = mapped_column(primary_key=True, autoincrement=False)
     name: Mapped[str] = mapped_column(String(20))
     genre: Mapped[GenreEnum] = mapped_column(Enum(GenreEnum))
-    image: Mapped[str] = mapped_column(String(255))
-    contour_points: Mapped[JSON] = mapped_column(JSON)
+    original_image: Mapped[str] = mapped_column(String(255))
+    badge_image: Mapped[str] = mapped_column(String(255))
+    contour_points: Mapped[list] = mapped_column(JSON)
